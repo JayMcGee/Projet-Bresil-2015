2eedd39daf121f21ab91fefb016277d3f5ba99a8
Cheroukee <fournierje.info@gmail.com>
2014-10-03 11:48:05 -0400

    Modifications pour compatibilité

--- Années Précédentes/Trie/Code/coordonateur_GPRS.X/build/default/production/commun.p1.d
+++
@@ -1,8 +0,0 @@
- build/default/production/commun.d  \
- build/default/production/commun.p1:  \
- commun.c  \
-usart.h  \
-bufferint.h  \
-GSM.h  \
-commun.h  \
-configuration_bits.c 
--- Années Précédentes/Trie/Code/coordonateur_GPRS.X/build/default/production/usart.p1.d
+++
@@ -1,8 +0,0 @@
- build/default/production/usart.d  \
- build/default/production/usart.p1:  \
- usart.c  \
-usart.h  \
-bufferint.h  \
-GSM.h  \
-commun.h  \
-configuration_bits.c 
