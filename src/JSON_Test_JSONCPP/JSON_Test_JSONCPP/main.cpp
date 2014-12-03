69ed360106b5a10d421bdfcd78b47727f3ac5617
cheroukee <fournierje.info@gmail.com>
2014-12-03 11:44:55 -0500

    Ajout du projet de gestion de fichier JSON pour les test (VS2013)

---
+++ src/JSON_Test_JSONCPP/JSON_Test_JSONCPP/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "json.h"
+
+using namespace std;
+using namespace json;
+
+int main(int argc, char **argv)
+{
+	cout << "Hello wordl" << endl;
+
+	system("pause");
+
+	return 0;
+}
